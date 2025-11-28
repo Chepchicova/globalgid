7dd59ac78c75a42d15be53aa17bd6ff62349000f
Chepchicova <elizabethchapukova@gmail.com>
2025-11-28 22:50:02 +0300

    Update db.php

--- public/backend/db.php
+++ public/backend/db.php
@@ -1,9 +1,6 @@
 <?php
 // параметры подключения
-$host = "localhost";      // сервер базы данных
-$user = "root";           // имя пользователя MySQL (по умолчанию root)
-$password = "Lerochka123";           // пароль (если есть — укажи)
-$dbname = "globalgid";    // имя твоей базы данных
+
 
 // подключение через mysqli
 $conn = new mysqli($host, $user, $password, $dbname);
